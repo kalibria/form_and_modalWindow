30e29f7fcc14db4e2484e6c082b4a70aa1048fc5
Maria Kazakova <mariakazakova92@gmail.com>
2024-02-29 13:50:45 +0300

    connected styles

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader',   // translates CSS into CommonJS
+          'sass-loader'   // compiles Sass to CSS
+        ]
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
