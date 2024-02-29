77cd3a97c92fe54765bdb439acdce5ee0a064831
Maria Kazakova <mariakazakova92@gmail.com>
2024-02-29 12:59:50 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  mode: 'development', // or 'production'
+  entry: './src/scripts/main.js', // Entry point of your application
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080 // or any other port you prefer
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+  ],
+};
