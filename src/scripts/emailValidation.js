4da90b3c00c5db34f285164ed3baa1681b5bbe22
Maria Kazakova <mariakazakova92@gmail.com>
2024-03-01 16:40:32 +0300

    refactored code

--- src/scripts/emailValidation.js
+++ src/scripts/emailValidation.js
@@ -1,9 +1,4 @@
 export const emailValidation = (email) =>{
   const regEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if(!regEmail.test(email)){
-    return false
-  }else {
-    alert("Email is valid.");
-    return true
-  }
+  return regEmail.test(email)
 }
--- src/scripts/nameValidation.js
+++ src/scripts/nameValidation.js
@@ -1,9 +1,4 @@
 export const nameValidation = (name) =>{
   const regName = /^[a-zA-Zа-яА-Я-']{2,}$/;
-  if(!regName.test(name)){
-    return false
-  }else {
-    alert('Valid name given.');
-    return true
-  }
+  return regName.test(name);
 }
